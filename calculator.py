09c4d56e5525ee12b5cd638def6fb6b566362075
TiffANY-JERuto <47849351+TiffANY-JERuto@users.noreply.github.com>
2019-04-24 13:21:39 +0300

    Calculator in python.

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def add(a,b):
+	answer= a+b
+	return answer
+
+def modulus(k,l):
+	answer=k%l
+	return answer
+
+
+def division(t,r):
+	answer=t/r
+	return answer
+
+
+def wholenumber(i,p):
+	answer=i//p
+	return answer
+
+
+def multiply(p,w):
+	answer= p*w 
+	return answer
+
+def percentage(a,b):
+	answer=(a/b)*100
+	return answer
+
+def subtract (a,b):
+	answer= a-b
+	return answer 
+
+def exponent(a,b):
+	answer= a**b
+	return answer
+
+
